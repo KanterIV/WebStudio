d45acd15c909e99d9764e2df147fbd9faa58b697
KanterIV <126972573+KanterIV@users.noreply.github.com>
2024-11-19 12:51:36 +0100

    fix modal close logic\bodyScrollLock

--- js/modal.js
+++ js/modal.js
@@ -7,8 +7,37 @@
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.modal.addEventListener('click', handleBackdropClick);
+  window.addEventListener('keydown', handleEscapePress);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    const isHidden = refs.modal.classList.toggle('is-hidden');
+    const scrollLockMethod = isHidden
+      ? 'enableBodyScroll'
+      : 'disableBodyScroll';
+
+    if (!isHidden) {
+      document.body.style.paddingRight = '16px';
+    } else {
+      document.body.style.paddingRight = '';
+    }
+    if (typeof bodyScrollLock !== 'undefined') {
+      bodyScrollLock[scrollLockMethod](document.body);
+    }
+  }
+
+  function handleBackdropClick(event) {
+    if (event.target === refs.modal) {
+      toggleModal();
+    }
+  }
+
+  function handleEscapePress(event) {
+    if (
+      event.code === 'Escape' &&
+      !refs.modal.classList.contains('is-hidden')
+    ) {
+      toggleModal();
+    }
   }
 })();
